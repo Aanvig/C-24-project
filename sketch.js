3b3948dfde1fb5b4c5b096edf4ba8dae7370bbf5
Aanvig <78943357+Aanvig@users.noreply.github.com>
2021-04-06 17:34:44 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	boxBottom=createSprite(width/2, height-50,200,20)
+	boxBottom.shapeColor="red"
+	boxLeftSide=createSprite(width/2-100, height-90,20,100)
+	boxLeftSide.shapeColor="red"
+	boxRightSide=createSprite(width/2+100, height-90,20,100)
+	boxRightSide.shapeColor="red"
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	paper = new Paper(200,200,25);
+
+	paper = Bodies.circle(200 , 200 , 5 , {restitution:0, isStatic:true});
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  //paper.display();
+
+  
+  
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if(keyCode === UP_ARROW) {
+		Body.applyForce(paper.body,paper.body.position,{x:85,y:-85})
+		
+	}
+}
+
